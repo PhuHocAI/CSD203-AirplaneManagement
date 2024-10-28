60d12f2d04a3d8f068b1dfcd6eefb8662f5b9de9
Phạm Anh Kiệt <phamanhkiet.dev@gmail.com>
2024-10-29 01:22:43 +0700

    fix Fe as required

--- airplane/src/components/Home.js
+++ airplane/src/components/Home.js
@@ -1,21 +1,14 @@
 import React from "react";
 import logo from "../assets/depositphotos_705817886-stock-illustration-commercial-aeroplane-icon-vector-illustration.jpg";
+import "./Home.css";
 function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh", // Thiết lập chiều cao view-port để nội dung có thể giữa khung theo chiều dọc
-        color: "blue",
-      }}
-    >
-      <img src={logo} alt="Logo" style={{ width: "150px" }} />{" "}
-      {/* Điều chỉnh kích thước logo theo nhu cầu */}
-      <h1>Welcome to Airport Management System</h1>
-      <p>This is a system to manage different aspects of airports.</p>
+    <div className="home-container">
+      <img src={logo} alt="Airplane Logo" className="airplane-logo" />
+      <h2 className="welcome-text">Welcome To The Airport Management System</h2>
+      <p className="welcome-text text-content">
+        This is a web application that allows you to manage your airport.
+      </p>
     </div>
   );
 }
